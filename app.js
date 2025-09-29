42d79a6bf227f530b714115b8529a44c7025e5e8
TonnyForGod <222011483@ump.ac.za>
2025-09-29 06:21:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import NewSignup from './pages/NewSignup'  // Add this import
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import NewSignup from './pages/NewSignup'; // Add this import
+import './App.css';
 
 function App() {
   return (
@@ -17,13 +17,13 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/new-signup" element={<NewSignup />} />  {/* Add this route */}
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/new-signup" element={<NewSignup />} /> {/* Add this line */}
           </Routes>
         </div>
       </Router>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
