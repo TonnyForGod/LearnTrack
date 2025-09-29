e05483d66f3e1b6d8d099b236a1e4cb7ebaa1c2e
TonnyForGod <222011483@ump.ac.za>
2025-09-29 04:28:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import './App.css'
+import TestSignup from './pages/TestSignup'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
+<Route path="/test-signup" element={<TestSignup />} />
           </Routes>
         </div>
       </Router>
